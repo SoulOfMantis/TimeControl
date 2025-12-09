be74a6a68351a6e25d14cbde87754e56e4412ca1
Андрей Тимофеев <timo@sfedu.ru>
2025-12-09 15:17:40 +0300

    Create C# proj and file

---
+++ TeamCity/FindPythogoreanTriples.cs
@@ -0,0 +1,15 @@
+﻿int n = 1000;
+for (int i = 1; i < n; i++)
+{
+	for (int j = i+1; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			if ((i*i + j*j) == (k*k))
+			{
+                Console.WriteLine($"{i}^2[{i*i}] + {j}^2[{j*j}] = {k}^2[{k*k}]");
+				break;
+            }
+        }
+	}
+}
