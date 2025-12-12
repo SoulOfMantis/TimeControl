886f0b4452d46c74f903a7c56e21c0da9a0357db
Andrey <timo@sfedu.ru>
2025-12-12 15:22:17 +0300

    Update .cs

--- TeamCity/FindPythogoreanTriples.cs
+++ TeamCity/FindPythogoreanTriples.cs
@@ -1,15 +1,28 @@
-﻿int n = 1000;
-for (int i = 1; i < n; i++)
+﻿using System;
+using System.IO;
+
+class Program
 {
-	for (int j = i+1; j < n; j++)
+	static void Main()
 	{
-		for (int k = 0; k < n; k++)
+		int n = 1000;
+		using (StreamWriter file = new StreamWriter("output.txt"))
 		{
-			if ((i*i + j*j) == (k*k))
+			for (int i = 1; i < n; i++)
 			{
-                Console.WriteLine($"{i}^2[{i*i}] + {j}^2[{j*j}] = {k}^2[{k*k}]")
-				break;
-            }
-        }
+				for (int j = i + 1; j < n; j++)
+				{
+					for (int k = 0; k < n; k++)
+					{
+						if ((i * i + j * j) == (k * k))
+						{
+							file.WriteLine($"{i}^2[{i * i}] + {j}^2[{j * j}] = {k}^2[{k * k}]");
+							break;
+						}
+					}
+				}
+			}
+		}
+
 	}
 }
