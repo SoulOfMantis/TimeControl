7e816cfed2126a291dc32e1fb05dd5466b4734f6
Andrey <timo@sfedu.ru>
2025-12-12 16:12:24 +0300

    add tests

--- TeamCity/FindPythogoreanTriples.cs
+++ TeamCity/FindPythogoreanTriples.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.IO;
-
-class Program
+﻿public class Program
 {
 	static void Main()
 	{
 		int n = 1000;
 		using (StreamWriter file = new StreamWriter("output.txt"))
 		{
-			for (int i = 1; i < n; i++)
+			for (int i = 1; i <= n; i++)
 			{
-				for (int j = i + 1; j < n; j++)
+				var restOfTriple = findPythogoreanTriple(i);
+
+                if (restOfTriple != (-1, -1))
 				{
-					for (int k = 0; k < n; k++)
-					{
-						if ((i * i + j * j) == (k * k))
-						{
-							file.WriteLine($"{i}^2[{i * i}] + {j}^2[{j * j}] = {k}^2[{k * k}]");
-							break;
-						}
-					}
-				}
-			}
+					var (j, k) = restOfTriple;
+                    file.WriteLine($"{i}^2[{i * i}] + {j}^2[{j*j}] = {k}^2[{k * k}]");
+                }
+            }
 		}
 
+	}
+
+	public static (int, int) findPythogoreanTriple(int a)
+	{
+		if (a <= 0) return (-1, -1);
+        for (int j = 1; j < a; j++)
+		{
+			for (int k = a; k < 2*a; k++)
+			{
+				if ((a * a + j * j) == (k * k)) return (j, k);
+			}
+		}
+        return (-1, -1);
 	}
 }
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestNegative()
+        {
+            Assert.That(Program.findPythogoreanTriple(-1) == (-1, -1));
+        }
+        [Test]
+        public void TestZero()
+        {
+            Assert.That(Program.findPythogoreanTriple(0) == (-1, -1));
+        }
+        [Test]
+        public void TestFirstPrimes()
+        {
+            Assert.That(Program.findPythogoreanTriple(2) == (-1, -1));
+            Assert.That(Program.findPythogoreanTriple(3) == (-1, -1));
+            Assert.That(Program.findPythogoreanTriple(4) == (3, 5));
+        }
+
+    }
+}
